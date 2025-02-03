8b0777c5ff5044908e26f4bb30aa2eb216c4f9db
jnakht <jisan.cse.cu@gmail.com>
2025-02-03 18:58:15 +0600

    all done without recheck

--- js/ai.js
+++ js/ai.js
@@ -1,17 +1,28 @@
 
 
 
-const loadData = async () => {
+const loadData = async (showMoreClicked) => {
     const res = await fetch('https://openapi.programming-hero.com/api/ai/tools');
     const data = await res.json();
     console.log(data.data);
     const AIs = data.data.tools;
-    showData(AIs);
+    showData(AIs, showMoreClicked);
 }
-loadData();
+loadData(false);
 
-const showData = (allData) => {
+
+
+const showData = (allData, showMoreClicked) => {
     const dataContainer = document.getElementById('data-container');
+    dataContainer.textContent = '';
+    const seeMoreButtonContainer = document.getElementById('see-more-button');
+    if (allData.length > 6 && !showMoreClicked) {
+        allData = allData.slice(0, 6);
+        seeMoreButtonContainer.classList.remove('hidden');
+    }
+    else {
+        seeMoreButtonContainer.classList.add('hidden');
+    }
     allData.forEach(data => {
         console.log(data);
         const newElement = document.createElement('div');
@@ -58,7 +69,7 @@ const handleDetails = async (id) => {
     newDiv.innerHTML = `
 
 
-                <div class="w-[70vw] border-2 border-green-500 mx-auto rounded-2xl bg-white h-[500px] flex justify-between gap-8 items-center">
+                <div class="w-[70vw] border-2 border-green-500 mx-auto rounded-2xl bg-white h-[500px] flex justify-around p-4 gap-8 items-center">
             <!-- left side container -->
             <div class="bg-[#EB575709] flex flex-col gap-6 flex-1 p-4">
                 <h3 class="text-[#111] text-2xl font-semibold">${data.data?.description}</h3>
@@ -74,25 +85,29 @@ const handleDetails = async (id) => {
                 <div>
                     <h2 class="text-[#111] text-2xl font-semibold">Features</h2>
                 <ul class="text-[#585858] text-base font-normal list-disc">
-                    <li>Customizable responses</li>
-                    <li>Multilingual support</li>
-                    <li>Seamless integration</li>
+                    <li>${data.data?.features?.[1]?.feature_name || 'N/A'}</li>
+                    <li>${data.data?.features?.[2]?.feature_name || 'N/A'}</li>
+                    <li>${data.data?.features?.[3]?.feature_name || 'N/A'}</li>
                 </ul>
                 </div>
                 <!-- Integration div -->
                 <div>
                     <h2 class="text-[#111] text-2xl font-semibold">Integrations</h2>
                     <ul class="text-[#585858] text-base font-normal list-disc">
-                        <li>FB Messenger</li>
-                        <li>Slack</li>
-                        <li>Telegram</li>
+                        <li>${data.data?.integrations
+                          ?.[0] || 'N/A'}</li>
+                    <li>${data.data?.integrations
+                      ?.[1] || 'N/A'}</li>
+                    <li>${data.data?.integrations
+                      ?.[2] || 'N/A'}</li>
                     </ul>
                 </div>
                 </div>
             </div>
             <!-- right side container -->
             <div class="flex-1 flex flex-col justify-between items-center">
-                <img src="./images/No-Image-Found-400x264.png" alt="">
+                
+                <img src="${data?.data?.image_link?.[0] || data?.data?.image_link?.[1] || data?.data?.image_link?.[2] || data?.data?.image_link?.[3]}" alt="" onerror="this.src='./images/No-Image-Found-400x264.png'" >
                 <div class="text-center mt-6 flex flex-col gap-4">
                     <h2 class="text-[#111] text-2xl font-semibold">Hi, how are you doing today?</h2>
                 <p class="text-[#585858] text-base font-light">I'm doing well, thank you for asking. How can I assist you today?</p>
@@ -108,4 +123,11 @@ const handleDetails = async (id) => {
 
     modalContentContainer.appendChild(newDiv);
     my_modal.showModal();
+}
+
+
+const handleSeeMoreButton = () => {
+    const seeMoreButtonContainer = document.getElementById('see-more-button');
+    loadData(true);
+
 }
